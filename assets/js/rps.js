a2248d4ee6feeb77e6d6979d00ed8775aead7226
madhavparekh <madhavparekh@gmail.com>
2018-03-06 10:00:22 -0800

    disp chat msg if players send same msg multiple times + other refinement

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -6,7 +6,13 @@ var player = {
 	loses: 0,
 	ties: 0,
 };
-var plyRef = 'player';
+var hasPlyPicked = false; //player pick flag
+var oppPicked = false; //opp pick flag
+var plyRef = 'player'; //ref for single player
+var ply = 0; //player # 1 or 2 to for firebase node
+var gameResult = ''; //string to hold game's result
+var oppName = ''; //opp's name
+var splitAt = `~\`~`; //msg string spliter
 
 var config = {
 	apiKey: 'AIzaSyCP_BeXBOanflUEwdoFCObZRXSNG1FD3Pc',
@@ -17,20 +23,15 @@ var config = {
 	messagingSenderId: '126843285379',
 };
 firebase.initializeApp(config);
-
-var dbPlayers = firebase.database().ref('players');
-var dbChat = firebase.database().ref('chat');
-var hasPlyPicked = false;
-var oppPicked = false;
-var ply = 0;
-var gameResult = '';
-var oppName = '';
+var dbPlayers = firebase.database().ref('players'); //players db
+var dbChat = firebase.database().ref('chat'); //chat db
 
 $('.alert').hide();
 
 $(document).ready(function (event) {
 	$('body').on('click', '#btn-name', function (event) {
-		player.name = $('#name').val().trim();
+		//gets name or assigns Annonymous if left empty
+		player.name = $('#name').val().trim() || 'Anonymous';
 		event.preventDefault();
 
 		//hide enter name area
@@ -67,68 +68,79 @@ $(document).ready(function (event) {
 			//add class for pulse animation
 			$(`.ply${ply}`).addClass('pulse');
 
+			//add .image${ply} to .image so this damn jquery works
+			$(`.image${ply}`).addClass('image');
+
+
 			//fade opp player's images/area
 			ply === 1 ? $('.ply2').fadeTo(500, 0.5) : $('.ply1').fadeTo(500, 0.5);
-			//on player's pick/click
-			$(`.image${ply}`).on('click', function (r) {
-				console.log('clicked');
-				if (!hasPlyPicked) {
-					hasPlyPicked = true;
-
-					//clone clicked img to play area
-					$(`#ply${ply}pick`).append(`Player ${ply}`, $(r.target).clone());
-					//remove pulsating animation
-					$(`.ply${ply}`).removeClass('pulse');
-
-					//add pulsating animation to opp side
-					$(`.ply${3 - ply}`).addClass('pulse');
-					//fade player's area
-					$(`.ply${ply}`).fadeTo(1000, 0.3);
-
-					//update remote Node
-					player.pick = $(r.target).attr('value');
-					var updatePick = {
-						pick: player.pick
-					};
-					dbPlayers.child(plyRef).update(updatePick);
-
-					if (oppPicked) {
-						dbPlayers.child(`player${3 - ply}/pick`).once('value', function (snap) {
-							console.log('line 114 ' + snap.val());
-							if (snap.val() != 3) {
-								console.log('checking winner from line 116');
-								dbPlayers.child(plyRef).update(checkWinner(snap.val()));
-							}
-
-						});
-					}
-				}
-			});
-
 		});
+	});
+
+	//on player's pick/click
+	$(`body`).on(`click`, `.image`, function (r) {
+		console.log('clicked');
+		if (!hasPlyPicked) {
+			hasPlyPicked = true;
+
+			//clone clicked img to play area
+			$(`#ply${ply}pick`).append(`Yours`, $(r.target).clone());
+			//remove pulsating animation
+			$(`.ply${ply}`).removeClass('pulse');
+
+			//add pulsating animation to opp side
+			$(`.ply${3 - ply}`).addClass('pulse');
+			//fade player's area
+			$(`.ply${ply}`).fadeTo(1000, 0.3);
+
+			//update remote Node
+			player.pick = $(r.target).attr('value');
+			var updatePick = {
+				pick: player.pick
+			};
+			dbPlayers.child(plyRef).update(updatePick);
+
+			if (oppPicked) {
+				dbPlayers.child(`player${3 - ply}/pick`).once('value', function (snap) {
+					console.log('line 114 ' + snap.val());
+					if (snap.val() != 3) {
+						console.log('checking winner from line 116');
+						dbPlayers.child(plyRef).update(checkWinner(snap.val()));
+					}
+				});
+			}
+		}
 	});
 
 	//on submit chat
 	$('body').on('click', '#btn-chat', function (e) {
-		// e.preventDefault();
-		var message = $('#btn-input').val()
-
-		console.log(message);
+		e.preventDefault();
+		//adds date string separator to create a new msg in case player enters 
+		//so that it updates firebase node and gets update @ on read 
+		//when entered same message by player
+		var message = `${$('#btn-input').val()} ${splitAt} ${new Date()}`;
 
 		dbChat.child(plyRef).set({
 			msg: message
 		})
-		insertChat(player.name, message, 'text-primary');
+		insertChat(player.name, message.split(splitAt)[0], 'text-primary');
 
 		$('#btn-input').val('');
+	});
 
-
+	//triggers 'enter' as submit chat btn
+	$('.input-group').keydown(function (e) {
+		var key = e.which;
+		if (key == 13) {
+			// As ASCII code for ENTER key is "13"
+			$('#btn-chat').trigger('click'); // Submit form code
+		}
 	});
 
 });
 
 function insertChat(name, msg, textColor) {
-	var chatDiv = $('<li>').addClass('p-0 m-0');
+	var chatDiv = $('<li>').addClass('p-0 ml-2');
 	var chatMsg = $(`<p>`).addClass(`${textColor} p-0 m-0`).html(`<b>${name}: </b>${msg}`);
 	chatDiv.append(chatMsg);
 	$(`ul`).append(chatDiv);
@@ -139,11 +151,10 @@ function chatListner() {
 	dbChat.child(`player${3 - ply}`).on('value', snap => {
 		try {
 			console.log(snap.val().msg);
-			insertChat(oppName, snap.val().msg, 'text-danger');
+			insertChat(oppName, snap.val().msg.split(splitAt)[0], 'text-danger');
 		} catch (error) {
 			//console.error(error);
 		}
-
 	}, function (errObj) {
 		console.log(errObj.code);
 	})
@@ -192,13 +203,12 @@ function listenForNodeChanges() {
 	);
 }
 
-
 function displayOppPick() {
 	if (hasPlyPicked && oppPicked) {
 
 		dbPlayers.child(`player${3 - ply}/pick`).once('value', function (snap) {
 
-			$(`#ply${3-ply}pick`).append(`Player${3-ply}`, $(`#${snap.val()}`).clone());
+			$(`#ply${3-ply}pick`).append($(`#${snap.val()}`).clone(), `${oppName}'s`);
 
 			$('#alertmsg').html(gameResult);
 
